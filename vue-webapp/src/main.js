a817281e397a28f6acd0a0dfd094bdfb4973f10c
niumx <mic7512@163.com>
2018-04-03 11:26:58 +0800

    vue 中引入element-ui

--- vue-webapp/src/main.js
+++ vue-webapp/src/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
